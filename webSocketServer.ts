296f89387f424d5450ae73132a4370edac11c375
arlgnv <lagunov.artamon@gmail.com>
2026-07-21 16:22:08 +0300

    feat: type websocket server

--- webSocketServer.ts
+++ webSocketServer.ts
@@ -1,7 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
 
-const matchSubscribers = new Map();
-
 function subscribe(matchId, socket) {
   if (!matchSubscribers.has(matchId)) {
     matchSubscribers.set(matchId, new Set());
@@ -60,75 +58,70 @@ function broadcastToMatch(matchId, payload) {
   });
 }
 
-function handleMessage(socket, data) {
-  let message;
-
-  try {
-    message = JSON.parse(data.toString());
-  } catch (error) {
-    sendJson(socket, { type: "error", message: "Invalid JSON" });
-    return;
-  }
-
-  if (message.type === "subscribe" && Number.isInteger(message.matchId)) {
-    subscribe(message.matchId, socket);
-    socket.subscriptions.add(message.matchId);
-    sendJson(socket, { type: "subscribed", matchId: message.matchId });
-
-    return;
-  }
-
-  if (message.type === "unsubscribe" && Number.isInteger(message.matchId)) {
-    unsubscribe(message.matchId, socket);
-    socket.subscriptions.delete(message.matchId);
-    sendJson(socket, { type: "unsubscribed", matchId: message.matchId });
-  }
+interface CustomWebSocket extends WebSocket {
+  alive: boolean;
 }
 
-const webSocketServer = new WebSocketServer({
+type CustomWebSocketConstructor = typeof WebSocket & {
+  new (): CustomWebSocket;
+};
+
+const webSocketServer = new WebSocketServer<CustomWebSocketConstructor>({
   noServer: true,
   path: "/ws",
   maxPayload: 1024 * 1024,
 });
 
-webSocketServer.on("connection", async (socket, req) => {
-  socket.isAlive = true;
-  socket.on("pong", () => {
-    socket.isAlive = true;
+webSocketServer.on("connection", async (ws) => {
+  ws.alive = true;
+
+  ws.on("pong", () => {
+    ws.alive = true;
   });
 
-  socket.subscriptions = new Set();
+  ws.on("message", (data) => {
+    let message;
 
-  sendJson(socket, { type: "welcome" });
+    try {
+      message = JSON.parse(data.toString());
+    } catch (error) {
+      sendJson(ws, { type: "error", message: "Invalid JSON" });
+      return;
+    }
 
-  socket.on("message", (data) => {
-    handleMessage(socket, data);
-  });
-
-  socket.on("error", () => {
-    socket.terminate();
-  });
-
-  socket.on("close", () => {
-    cleanupSubscriptions(socket);
-  });
-
-  socket.on("error", console.error);
-});
-
-const interval = setInterval(() => {
-  webSocketServer.clients.forEach((client) => {
-    if (client.isAlive === false) {
-      client.terminate();
+    if (message.type === "subscribe" && Number.isInteger(message.matchId)) {
+      subscribe(message.matchId, ws);
+      ws.subscriptions.add(message.matchId);
+      sendJson(ws, { type: "subscribed", matchId: message.matchId });
 
       return;
     }
 
-    client.isAlive = false;
-    client.ping();
+    if (message.type === "unsubscribe" && Number.isInteger(message.matchId)) {
+      unsubscribe(message.matchId, ws);
+      ws.subscriptions.delete(message.matchId);
+      sendJson(ws, { type: "unsubscribed", matchId: message.matchId });
+    }
+  });
+
+  ws.on("error", console.error);
+});
+
+const interval = setInterval(() => {
+  webSocketServer.clients.forEach((ws) => {
+    if (ws.alive === false) {
+      ws.terminate();
+
+      return;
+    }
+
+    ws.alive = false;
+    ws.ping();
   });
 }, 30_000);
 
-webSocketServer.on("close", () => clearInterval(interval));
+webSocketServer.on("close", () => {
+  clearInterval(interval);
+});
 
 export default webSocketServer;
